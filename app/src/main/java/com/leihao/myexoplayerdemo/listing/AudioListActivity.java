8b9ccbab4e52d5d9fc887b013e6a3d23ed529612
Mark <lh.t-mac@qq.com>
2019-03-28 17:29:48 +0800

    增加通知栏后台播放

--- app/src/main/java/com/leihao/myexoplayerdemo/listing/AudioListActivity.java
+++ app/src/main/java/com/leihao/myexoplayerdemo/listing/AudioListActivity.java
@@ -1,6 +1,8 @@
 package com.leihao.myexoplayerdemo.listing;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 import com.leihao.myexoplayerdemo.R;
 import com.leihao.myexoplayerdemo.util.ActivityUtils;
@@ -24,5 +26,17 @@ public class AudioListActivity extends DaggerAppCompatActivity {
             audioListFragment = fragment;
             ActivityUtils.addFragmentToActivity(getSupportFragmentManager(), audioListFragment, R.id.fragment_listing);
         }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {//设置返回按钮：不应该退出程序---而是返回桌面
+            Intent home = new Intent(Intent.ACTION_MAIN);
+//            home.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            home.addCategory(Intent.CATEGORY_HOME);
+            startActivity(home);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
--- app/src/main/java/com/leihao/myexoplayerdemo/listing/AudioListFragment.java
+++ app/src/main/java/com/leihao/myexoplayerdemo/listing/AudioListFragment.java
@@ -1,9 +1,19 @@
 package com.leihao.myexoplayerdemo.listing;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -11,12 +21,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.ui.PlayerNotificationManager;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
@@ -35,6 +49,8 @@ import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import dagger.android.support.DaggerFragment;
 
+import static android.content.Context.NOTIFICATION_SERVICE;
+
 @ActivityScoped
 public class AudioListFragment extends DaggerFragment implements AudioListContract.View {
 
@@ -48,10 +64,13 @@ public class AudioListFragment extends DaggerFragment implements AudioListContra
 
     private DataSource.Factory dataSourceFactory;
     private SimpleExoPlayer player;
+    private PlayerNotificationManager playerNotificationManager;
 
     private List<AudioBean> audioBeans = new ArrayList<>();
     private AudioListingAdapter adapter;
     private Unbinder unbinder;
+
+    private final String channelId = "voice_play";
 
     @Inject
     public AudioListFragment() {
@@ -93,12 +112,100 @@ public class AudioListFragment extends DaggerFragment implements AudioListContra
         dataSourceFactory = new DefaultHttpDataSourceFactory(Util.getUserAgent(getContext(), "MyExoPlayerDemo"));
         player.setRepeatMode(Player.REPEAT_MODE_ALL);
         player_bar.setPlayer(player);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(channelId, "音频播放", NotificationManager.IMPORTANCE_HIGH, null);
+        }
+        playerNotificationManager = new PlayerNotificationManager(
+                getContext(),
+                channelId,
+                1,
+                new DescriptionAdapter());
+        playerNotificationManager.setPlayer(player);
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    void createNotificationChannel(String id, String name, int importance, String desc) {
+        NotificationManager notificationManager = (NotificationManager) getContext().getSystemService(NOTIFICATION_SERVICE);
+        if (notificationManager.getNotificationChannel(id) != null)
+            return;
+        NotificationChannel notificationChannel = new NotificationChannel(id, name, importance);
+        notificationChannel.enableLights(false);
+        notificationChannel.enableVibration(false);
+
+        notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+        notificationChannel.setBypassDnd(true);
+        notificationChannel.setDescription(desc);
+
+        notificationManager.createNotificationChannel(notificationChannel);
     }
 
     @Override
     public void onResume() {
         super.onResume();
         mPresenter.loadAudioList();
+        if (player != null
+                && player.getPlaybackState() != Player.STATE_IDLE
+                && player.getPlaybackState() != Player.STATE_ENDED
+                && player.getPlayWhenReady()) {
+            player_bar.setVisibility(View.VISIBLE);
+        } else {
+            player_bar.setVisibility(View.GONE);
+        }
+    }
+
+    private String getTitle(int window) {
+        if (window >= audioBeans.size()) {
+            return null;
+        }
+        AudioBean bean = audioBeans.get(window);
+        return bean.speechName;
+    }
+
+    private String getDescription(int window) {
+        if (window >= audioBeans.size()) {
+            return null;
+        }
+        AudioBean bean = audioBeans.get(window);
+        return bean.speechDesc;
+    }
+
+    private String getLargeIconUri(int window) {
+        if (window >= audioBeans.size()) {
+            return null;
+        }
+        AudioBean bean = audioBeans.get(window);
+        return bean.speechImage;
+    }
+
+    private void loadBitmap(String iconUri, PlayerNotificationManager.BitmapCallback callback) {
+        Glide.with(getContext())
+                .asBitmap()
+                .load(iconUri)
+                .into(new SimpleTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                        callback.onBitmap(resource);
+                    }
+                });
+    }
+
+    private Bitmap getPlaceholderBitmap() {
+        return BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+    }
+
+    private PendingIntent createPendingIntent(int window) {
+        if (window >= audioBeans.size()) {
+            return null;
+        }
+        AudioBean bean = audioBeans.get(window);
+        // Create an Intent for the activity you want to start
+        Intent resultIntent = new Intent(getContext(), AudioListActivity.class);
+        // Create the TaskStackBuilder and add the intent, which inflates the back stack
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(getContext());
+        stackBuilder.addNextIntentWithParentStack(resultIntent);
+        // Get the PendingIntent containing the entire back stack
+        return stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
     @Override
@@ -134,10 +241,48 @@ public class AudioListFragment extends DaggerFragment implements AudioListContra
         return isAdded();
     }
 
+    private class DescriptionAdapter implements PlayerNotificationManager.MediaDescriptionAdapter {
+
+        @Override
+        public String getCurrentContentTitle(Player player) {
+            int window = player.getCurrentWindowIndex();
+            return getTitle(window);
+        }
+
+        @Nullable
+        @Override
+        public String getCurrentContentText(Player player) {
+            int window = player.getCurrentWindowIndex();
+            return getDescription(window);
+        }
+
+        @Nullable
+        @Override
+        public Bitmap getCurrentLargeIcon(Player player, PlayerNotificationManager.BitmapCallback callback) {
+            int window = player.getCurrentWindowIndex();
+            if (getLargeIconUri(window) != null) {
+                // load bitmap async
+                loadBitmap(getLargeIconUri(window), callback);
+                return getPlaceholderBitmap();
+            }
+            return null;
+        }
+
+        @Nullable
+        @Override
+        public PendingIntent createCurrentContentIntent(Player player) {
+            int window = player.getCurrentWindowIndex();
+            return createPendingIntent(window);
+        }
+    }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         mPresenter.dropView();
         unbinder.unbind();
+        playerNotificationManager.setPlayer(null);
+        player.release();
+        player = null;
     }
 }
